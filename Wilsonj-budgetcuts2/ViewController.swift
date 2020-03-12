2e71b1550bf4303987a3881d51a450691a9505a9
Jacob Wilson <jacoblwilson.cs@gmail.com>
2020-03-11 20:34:30 -0500

    Initial Commit

---
+++ Wilsonj-budgetcuts2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wilsonj-budgetcuts2
+//
+//  Created by Jacob Wilson on 3/11/20.
+//  Copyright © 2020 Jacob Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
